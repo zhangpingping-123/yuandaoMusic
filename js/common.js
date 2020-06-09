1d4d3a07b9296556d5fc8e382c1ac0c785f509c8
zhangpingping-123 <2569228717@qq.com>
2020-06-09 16:28:04 +0800

    update

---
+++ js/common.js
@@ -0,0 +1,28 @@
+(function(Vue,axios){
+    const Axios=axios.create({
+        //1.配置域名
+        baseURL:'https://apimusic.linweiqin.com',
+        //请求时长
+        timeout:10000,
+    });
+    //添加请求拦截
+    Axios.interceptors.request.use(function (config) {
+        // Do something before request is sent
+        // console.log(config)
+        return config;
+      }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+      });
+      //返回拦截器
+      Axios.interceptors.response.use(function (response) {
+        //只获取到数据中的data部分
+        // console.log(response)
+        return response.data;
+      }, function (error) {
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        return Promise.reject(error);
+      });
+      window.Axios = Axios;
+})(Vue,axios)
